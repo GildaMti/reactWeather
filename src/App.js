54fe159f434f56d043cdf0b5e808c7ac55d50f14
GildaMti <89417592+GildaMti@users.noreply.github.com>
2021-11-14 13:20:03 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <SearchEngine />
+     <footer>
+        <br />
+        This app was coded by Mozhdeh Matalebi and is open-sourced on
+        <a
+          href="https://github.com/GildaMti/reactWeather"
+          target="_blank"
+          rel="noreferrer"
+        >
+           Github
+        </a>
+      </footer>
     </div>
   );
 }
